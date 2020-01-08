83106d5eb1bded74d6852c4acafba8cd61193028
Freezing Snail <36997863+FreezeSnail@users.noreply.github.com>
2020-01-07 21:29:05 -0500

    implemented global variables
    chpt. 21

--- cLox/compiler.c
+++ cLox/compiler.c
@@ -32,7 +32,7 @@ typedef enum {
   PREC_PRIMARY                  
 } Precedence; 
 
-typedef void (*ParseFn)();
+typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
     ParseFn prefix;
@@ -151,7 +151,20 @@ static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
-static void binary(){
+static uint8_t identifierConstant(Token* name){
+    return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage){
+    consume(TOKEN_IDENTIFIER, errorMessage);
+    return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global){
+    emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+static void binary(bool canAssign){
     //remember the operator
     TokenType operatorType = parser.previous.type;
 
@@ -176,7 +189,7 @@ static void binary(){
     }
 }
 
-static void literal(){
+static void literal(bool canAssign){
     switch(parser.previous.type){
         case TOKEN_FALSE: emitByte(OP_FALSE); break;
         case TOKEN_TRUE:  emitByte(OP_TRUE); break;
@@ -186,21 +199,36 @@ static void literal(){
     }
 }
 
-static void grouping(){
+static void grouping(bool canAssign){
     expression();
     consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
 }
 
-static void number(){
+static void number(bool canAssign){
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
 }
 
-static void string(){
+static void namedVariable(Token name, bool canAssign){
+    uint8_t arg = identifierConstant(&name);
+    
+    if(canAssign && match(TOKEN_EQUAL)){
+        expression();
+        emitBytes(OP_SET_GLOBAL, arg);
+    } else {
+        emitBytes(OP_GET_GLOBAL, arg);
+    }
+}
+
+static void string(bool canAssign){
     emitConstant(OBJ_VAL(copyString(parser.previous.start+1, parser.previous.length -2)));
 }
 
-static void unary(){
+static void variable(bool canAssign){
+    namedVariable(parser.previous, canAssign);
+}
+
+static void unary(bool canAssign){
     TokenType operatorType = parser.previous.type;
 
     //compile operand
@@ -235,7 +263,7 @@ ParseRule rules[] = {
   { NULL,     binary,  PREC_COMPARISON }, // TOKEN_GREATER_EQUAL   
   { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS            
   { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS_EQUAL      
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
+  { variable, NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
   { string,   NULL,    PREC_NONE },       // TOKEN_STRING          
   { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER          
   { NULL,     NULL,    PREC_NONE },       // TOKEN_AND             
@@ -266,12 +294,17 @@ static void parsePrecedence(Precedence precedence){
         return;
     }
 
-    prefixRule();
+    bool canAssign = precedence <= PREC_ASSIGNMENT;
+    prefixRule(canAssign);
 
     while(precedence <= getRule(parser.current.type)->precedence){
         advance();
         ParseFn infixRule = getRule(parser.previous.type)->infix;
-        infixRule();
+        infixRule(canAssign);
+    }
+
+    if(canAssign && match(TOKEN_EQUAL)){
+        error("Invalid assignment.");
     }
 }
 
@@ -281,6 +314,20 @@ static ParseRule* getRule(TokenType type){
 
 static void expression(){
     parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void varDeclaration(){
+    uint8_t global = parseVariable("Expect variable name");
+
+    if(match(TOKEN_EQUAL)){
+        expression();
+    } else {
+        emitByte(OP_NIL);
+    }
+
+    consume(TOKEN_SEMICOLON, "Expect ';' after variable declaration");
+
+    defineVariable(global);
 }
 
 static void expressionStatement(){
@@ -321,7 +368,11 @@ static void synchronize(){
 }
 
 static void declaration(){
-    statement();
+    if(match(TOKEN_VAR)){
+        varDeclaration();
+    } else {
+        statement();
+    }
 
     if(parser.panicMode) synchronize();
 }
